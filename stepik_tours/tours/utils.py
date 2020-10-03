50aeb80782f3498b47d7cf29ea3c3b57964175fd
gitSergeyhab <redmoloch@yandex.ru>
2020-10-03 15:29:38 +0300

    kill re, add kwargs

--- stepik_tours/tours/utils.py
+++ stepik_tours/tours/utils.py
@@ -3,32 +3,28 @@ from .data import *
 
 
 # рандомные тэги и отели
-def random_tegs_hotels(list_val, num):
-    ran = list(range(1, len(list_val)))
+def random_tags_hotels(list_values, num):
+    ran = list(range(1, len(list_values)))
     shuffle(ran)
-    return [list_val[lv] for lv in range(len(list_val)) if lv in ran[:num]]
+    return [list_values[lv] for lv in range(len(list_values)) if lv in ran[:num]]
 
 
 # расчет минимальной и максимальной цены/дней
 def minimax(tours_from_one_city, category):
-    minc, maxc = 1_000_000_000, -1
+    some_min, some_max = 1_000_000_000, -1
     for i in tours_from_one_city.values():
-        if i[category] > maxc:
-            maxc = i[category]
-        if i[category] < minc:
-            minc = i[category]
-    return (minc, maxc)
+        if i[category] > some_max:
+            some_max = i[category]
+        if i[category] < some_min:
+            some_min = i[category]
+    return (some_min, some_max)
 
 
 # dict туров для конкретного города !!
-def fun_tours_x(rp):
-    tours_x = {}
-    for tk, tv in tours.items():
-        if tv["departure"] == rp:
-            tours_x[tk] = tv
-    return tours_x
-#
-# # !!
-def bad_re(rp):
-    import re
-    return re.findall(pattern=r"\/[^\/]+\/$", string=rp)[-1][1:-1]
+def fun_one_city_tours(slug):
+    one_city_tours = {}
+    for number, data in tours.items():
+        if data["departure"] == slug:
+            one_city_tours[number] = data
+    return one_city_tours
+
--- stepik_tours/tours/views.py
+++ stepik_tours/tours/views.py
@@ -9,7 +9,7 @@ class MainView(View):
             request, 'tours/index.html', context={
                 'tours': tours,
                 'list_variables': list_variables,
-                'ran': random_tegs_hotels(list(range(1, 17)), 6),  # рандомные отели в нижней части главной странице
+                'ran': random_tags_hotels(list(range(1, 17)), 6),  # рандомные отели в нижней части главной странице
                 'cities': cities,
             }
         )
@@ -17,8 +17,8 @@ class MainView(View):
 
 class DepartureView(View):
     def get(self, request, *args, **kwargs):
-        rp_city = bad_re(request.path)
-        one_city_tours = fun_tours_x(rp_city)  # dict туров для конкретного города
+        city_slug = self.kwargs['departure']
+        one_city_tours = fun_one_city_tours(city_slug)
         prices = minimax(one_city_tours, "price")
         nights = minimax(one_city_tours, "nights")
         return render(request, 'tours/departure.html',
@@ -28,24 +28,24 @@ class DepartureView(View):
                           'list_variables': list_variables,
                           'prices': prices,
                           "days": nights,
-                          'rp_city': rp_city,  # хвост request.path
+                          'city_slug': city_slug,
                           'one_city_tours': one_city_tours,
-                          'city': cities[rp_city]  # город по хвосту request.path
+                          'city': cities[city_slug],
                       }
                       )
 
 
 class TourView(View):
     def get(self, request, *args, **kwargs):
-        rp_city = int(bad_re(request.path))
-        tour_from_one_city = tours[rp_city]
+        city_pk = self.kwargs['pk']
+        tour_from_one_city = tours[city_pk]
         return render(request, 'tours/tour.html',
                       context={
                           'cities': cities,
                           'list_variables': list_variables,
-                          'rp_city': rp_city,
+                          'city_pk': city_pk,
                           'tour_from_one_city': tour_from_one_city,  # тур по номеру из хвоста request.path
-                          'tags1': random_tegs_hotels(random_tag_words, 3),  # рандомные тэги
+                          'tags1': random_tags_hotels(random_tag_words, 3),  # рандомные тэги
                           'city': cities[tour_from_one_city["departure"]]  # из какого города
                       }
                       )
